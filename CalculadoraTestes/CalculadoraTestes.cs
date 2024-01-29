5dc699467b21d10deda5ef4ecf5458d93b6500c9
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-29 15:10:36 -0400

    testando arrays

--- CalculadoraTestes/CalculadoraTestes.cs
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -21,6 +21,40 @@ public class CalculadoraTestes
         int resultado = _calc.Somar(num1, num2);
         //Assert
         Assert.Equal(15, resultado);
-
     }
+    [Fact]
+    public void DeveVerificarSe4EhParERetornarVerdadeiro()
+    {
+        //Arrange
+        int numero = 4;
+        //Act
+        bool resultado = _calc.EhPar(numero);
+        //Assert
+        Assert.True(resultado);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    public void DeveVerificarSeOsNumerosSaoParesERetornarVerdadeiro(int numero)
+    {
+        //Act
+        bool resultado = _calc.EhPar(numero);
+
+        //Assert
+        Assert.True(resultado);
+    }
+
+    [Theory]
+    [InlineData(new int[] {2, 4} )]
+    [InlineData(new int[] {6, 8, 10} )]
+    public void DeveVerificarSeOsNumerosDosArraysSaoParesERetornarVerdadeiro(int[] numeros)
+    {
+        //Assert / Act
+       Assert.All(numeros, x => Assert.True(_calc.EhPar(x)));
+    }
+
 }
