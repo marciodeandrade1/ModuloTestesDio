65225437b6c309227d8c6488b5d511a4ac38b8e8
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-29 14:25:19 -0400

    Exemplo Teste Unitário de um método com xUnit

---
+++ Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,10 @@
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
