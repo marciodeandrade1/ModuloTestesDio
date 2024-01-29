ebadabdb8b9f9324129ef24b357c919f21880bc7
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-29 14:46:25 -0400

    Atualizações

--- Calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65225437b6c309227d8c6488b5d511a4ac38b8e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CalculadoraTestes/obj/Debug/net8.0/calculadoratestes.AssemblyInfo.cs
+++ CalculadoraTestes/obj/Debug/net8.0/calculadoratestes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadoratestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65225437b6c309227d8c6488b5d511a4ac38b8e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadoratestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadoratestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
